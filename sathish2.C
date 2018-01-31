286317284ca6b87268fc0ca7f7256f6cee63022d
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-01-31 16:47:46 +0530

    sathish2
    sathish2

---
+++ sathish2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("Enter the number"\n);
+scanf("%d",&a);
+if(a%==0)
+printf("The number is even"\n);
+else
+printf("The number is odd"\n);
+getch();
+return 0;
+}
