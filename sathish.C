1b7d787a5cc359ad55496d0431b593ba60ebc532
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-01-31 16:23:16 +0530

    sathish1
    sathish

---
+++ sathish.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("Enter the number"\n);
+scanf("%d",&a);
+if(a>0)
+{
+printf("The number is positive"\n);
+}
+else(a<0)
+{
+printf("The number is negative");
+}
+getch();
+return 0;
+}
